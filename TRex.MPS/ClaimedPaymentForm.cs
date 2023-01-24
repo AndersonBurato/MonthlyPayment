4f3d7373b4fb40c84fbbeb4fa716f310a8c8ded5
Anderson Burato <anderson.burato@gmail.com>
2023-01-24 19:21:38 -0300

    claimed payment form

---
+++ TRex.MPS/ClaimedPaymentForm.cs
@@ -0,0 +1,18 @@
+﻿namespace TRex.MPS;
+
+public partial class ClaimedPaymentForm : Form
+{
+    private string[,] magnifiedNumbers =
+    {
+        { "  _ ", "   ", "  _ ", " _ ", "    ", "  _ ", "  _ ", " _ ", "  _ ", "  _ " },
+        { " | |", " | ", "  _|", " _|", " |_|", " |_ ", " |_ ", "  |", " |_|", " |_|" },
+        { " |_|", " | ", " |_ ", " _|", "   |", "  _|", " |_|", "  |", " |_|", "  _|" }
+    };
+
+    public ClaimedPaymentForm()
+    {
+        InitializeComponent();
+    }
+
+    //generate a report with all the payments claimed or pending
+}
